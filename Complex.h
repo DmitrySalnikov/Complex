2be6119d7904ce0c48b8df92389932a1829494d4
DmitrySalnikov <mejibkop.ru@mail.ru>
2015-05-18 01:25:20 +0300

    remake

--- Complex.h
+++ Complex.h
@@ -1,23 +1,32 @@
-#ifndef HEADER_
-#define HEADER_
+#pragma once
 
-#include "io.h"
+#include <iostream>
 
 class Complex {
-	double x;
-	double y;
+private:
+	double Re_;
+	double Im_;
 public:
-	Complex(double a=0, double b=0) {x=a; y=b;}
-	~Complex() {}
-	Complex operator+(const Complex & z) const {return Complex(x+z.x, y+z.y);}
-	Complex operator-(const Complex & z) const {return Complex(x-z.x, y-z.y);}
-	Complex operator*(const Complex & z) const {return Complex(x*z.x-y*z.y, x*z.y+y*z.x);}
-	Complex operator*(double a) const {return Complex(x*a, y*a);}
-	Complex operator~() const {return Complex(x, -y);}
-	friend ostream & operator<<(ostream &, const Complex &);
-	friend istream & operator>>(istream &, Complex &);
+	Complex(double Re = 0, double Im = 0) : Re_(Re), Im_(Im) {}
+	Complex operator+(const Complex & z) const { return Complex(Re_+z.Re_, Im_+z.Im_); }
+	Complex operator-(const Complex & z) const { return Complex(Re_-z.Re_, Im_-z.Im_); }
+	Complex operator*(const Complex & z) const { return Complex(Re_*z.Re_-Im_*z.Im_, Re_*z.Im_+Im_*z.Re_); }
+	Complex operator*(double a) const { return Complex(Re_*a, Im_*a); }
+	Complex operator~() const { return Complex(Re_, -Im_); }
+    double Re() const { return Re_; }
+    double & Re() { return Re_; }
+    double Im() const { return Im_; }
+    double & Im() { return Im_; }
+    friend Complex operator*(double a, const Complex & z) { return z * a; }
+	friend std::ostream & operator<<(std::ostream & out, const Complex & z) {
+    	out << "(" << z.Re_ << ", " << z.Im_ << ")";
+	    return out;
+    }
+	friend std::istream & operator>>(std::istream & in, Complex & z) {
+	    in >> z.Re_;
+    	if (!in)
+	    	return in;
+    	in >> z.Im_;
+	    return in;
+    }
 };
-
-Complex operator*(double, const Complex &);
-
-#endif
--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include "Complex.h"
+#include <iostream>
 
 int main() {
+    using std::endl;
 	Complex a(3.0, 4.0);
 	Complex c;
-	cout<<"Enter complex number: \n";
-	while (cin>>c) 
-		cout<<c<<endl
-			<<~c<<endl
-			<<a<<endl
-			<<a+c<<endl
-			<<a-c<<endl
-			<<a*c<<endl
-			<<2*c<<endl
-			<<"Enter next complex number: \n";
+    std::cout << "Enter complex number: \n";
+	while (std::cin >> c) 
+		std::cout << c << endl
+			<< ~c << endl
+			<< a << endl
+			<< a + c << endl
+			<< a - c << endl
+			<< a * c << endl
+			<< 2 * c << endl
+			<< "Enter next complex number: \n";
     
     return 0;
 }
