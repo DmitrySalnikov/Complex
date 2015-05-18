4c061d930c8e3f6c09cdddf548c6b38bc73d4857
DmitrySalnikov <mejibkop.ru@mail.ru>
2015-05-18 14:56:57 +0300

    friend

--- Complex.h
+++ Complex.h
@@ -8,17 +8,32 @@ private:
 	double Im_;
 public:
 	Complex(double Re = 0, double Im = 0) : Re_(Re), Im_(Im) {}
-	Complex operator+(const Complex & z) const { return Complex(Re_+z.Re_, Im_+z.Im_); }
-	Complex operator-(const Complex & z) const { return Complex(Re_-z.Re_, Im_-z.Im_); }
-	Complex operator*(const Complex & z) const { return Complex(Re_*z.Re_-Im_*z.Im_, Re_*z.Im_+Im_*z.Re_); }
-	Complex operator*(double a) const { return Complex(Re_*a, Im_*a); }
+	Complex & operator+=(const Complex & z) { Re_ += z.Re_; Im_ += z.Im_; return *this; }
+	Complex & operator-=(const Complex & z) { Re_ -= z.Re_; Im_ -= z.Im_; return *this; }
+	Complex & operator*=(const Complex & z) { 
+        double re_ = Re_; 
+        Re_ = Re_*z.Re_-Im_*z.Im_; 
+        Im_ = re_*z.Im_+Im_*z.Re_; 
+        return *this; 
+    }
 	Complex operator~() const { return Complex(Re_, -Im_); }
     double Re() const { return Re_; }
     double & Re() { return Re_; }
     double Im() const { return Im_; }
     double & Im() { return Im_; }
-    friend Complex operator*(double a, const Complex & z) { return z * a; }
-	friend std::ostream & operator<<(std::ostream & out, const Complex & z) {
+	friend Complex operator+(Complex const & a, Complex const & z) { 
+        Complex t = a;
+        return t += z; 
+    }
+	friend Complex operator-(Complex const & a, Complex const & z) { 
+        Complex t = a;
+        return t -= z; 
+    }
+    friend Complex operator*(Complex const & a, Complex const & z) { 
+        Complex t = a;
+        return t *= z; 
+    }
+	friend std::ostream & operator<<(std::ostream & out, Complex const & z) {
     	out << "(" << z.Re_ << ", " << z.Im_ << ")";
 	    return out;
     }
--- main.cpp
+++ main.cpp
@@ -3,17 +3,18 @@
 
 int main() {
     using std::endl;
-	Complex a(3.0, 4.0);
+	Complex a(3, 4.0);
 	Complex c;
     std::cout << "Enter complex number: \n";
 	while (std::cin >> c) 
-		std::cout << c << endl
+		std::cout << a << endl
+			<< c << endl
 			<< ~c << endl
-			<< a << endl
 			<< a + c << endl
 			<< a - c << endl
 			<< a * c << endl
 			<< 2 * c << endl
+			<< 2 + c << endl
 			<< "Enter next complex number: \n";
     
     return 0;
